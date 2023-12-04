7e76caf06f43e31274b0d51ddc38de0e53af9009
준기 <doxie@jungiui-MacBookAir.local>
2023-12-04 12:10:40 +0900

    asdf

--- api/getProfile.ts
+++ api/getProfile.ts
@@ -1,14 +1,14 @@
+import axios from "axios";
 import { getCookie } from "cookies-next";
 import Router from "next/router";
 
 const getProfile = async () => {
   try {
-    const { data } = await fetch("/auth/google", {
+    const { data } = await axios.get("/auth/google", {
       headers: {
         Authorization: `Bearer ${getCookie("accessToken")}`,
       },
-    }).then((response) => response.json());
-
+    });
     return data;
   } catch {
     if (Router.pathname !== "/login" && Router.pathname !== "/") {
